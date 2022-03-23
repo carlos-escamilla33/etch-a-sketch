6058c4c4a567f5d940531ee57c583148b53612c4
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-03-22 19:16:19 -0700

    Make game container div non resizeable

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const gameContainer = document.querySelector("#game-container");
 
-const createGrid = () => {
-    // later on we can add a parameter so we can set the number of sqaures
-    for (let i = 0; i < 16; i++) {
+
+const game = () => {
+    createGrid(16, 16);
+}
+
+const createGrid = (rows, cols) => {
+    gameContainer.style.setProperty("--grid-rows", rows);
+    gameContainer.style.setProperty("--grid-columns", cols);
+    for (let i = 0; i < (rows * cols); i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.textContent = (i + 1);
         gameContainer.append(square);
     }
 }
 
-createGrid();
+game();
