507c410d6fdc97a7cd838c8dc7499c90ec81eb26
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2022-08-17 13:38:27 -0700

    fixed game container resize issue

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 const gameContainer = document.querySelector("#game-container");
 const gridSizeBtn = document.querySelector("#btn");
 
-const onDrag = (event) => {
-    const gridSquare = document.querySelector(".square");
+const onHover = (event) => {
     if (event.target.className == "square") {
         event.target.style.backgroundColor = "yellow";
     };
 };
 
-const createGrid = (rows, cols) => {
+const createGrid = (rows=16, cols=16) => {
     gameContainer.style.setProperty("--grid-rows", rows);
     gameContainer.style.setProperty("--grid-columns", cols);
     for (let i = 0; i < (rows * cols); i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         gameContainer.append(square);
-        square.addEventListener("dragover", onDrag);
+        square.addEventListener("mouseover", onHover);
     };
 };
 
 const resizeGrid = () => {
 
-    let rows = parseInt(prompt("Enter number of rows: "));
-    let columns = parseInt(prompt("Enter number of columns"));
-
-    createGrid(50, 50);
 };
 
 const game = () => {
-    createGrid(50, 50);
+    createGrid();
 };
 
 game();
