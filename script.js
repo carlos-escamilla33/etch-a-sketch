cd58da374a230f94efbceeca301f65a35853ddbf
Carlos Escamilla <Crodriguezescamilla3@gmail.com>
2023-03-11 00:31:17 -0800

    added board resizing

--- script.js
+++ script.js
@@ -3,36 +3,46 @@ const gridSizeBtns = document.querySelectorAll(".btn");
 let cols;
 let rows;
 
-const onHover = (event) => {
+const onHoverSizeBtns = (event) => {
     if (event.target.className == "square") {
-        event.target.style.backgroundColor = "purple";
+        event.target.style.backgroundColor = "pink";
     };
 };
 
-const createGrid = (rows=16, cols=16) => {
+const resizeGrid = (event) => {
+    reset();
+    if (event.target.className === "btn sm"){
+        cols = 16, rows = 16;
+    } else if (event.target.className === "btn md") {
+        cols = 32, rows = 32;
+    } else if (event.target.className === "btn lg") {
+        cols = 64, rows = 64;
+    }
+    createGrid(rows, cols);
+};
+
+const createGrid = (rows, cols) => {
     gameContainer.style.setProperty("--grid-rows", rows);
     gameContainer.style.setProperty("--grid-columns", cols);
     for (let i = 0; i < (rows * cols); i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         gameContainer.append(square);
-        square.addEventListener("mouseover", onHover);
+        square.addEventListener("mouseover", onHoverSizeBtns);
     };
 };
 
-const resizeGrid = (event) => {
-    if (event.target.className === "btn sm"){
-        cols = 16, rows = 16;
-    } else if (event.target.className === "btn md") {
-        cols = 50, rows = 50;
-    } else if (event.target.className === "btn lg") {
-        cols = 100, rows = 100;
-    }
-};
+const reset = () => {
+    for (let i = 0; i < (rows * cols); i++) {
+        const square = document.querySelector(".square");
+        gameContainer.removeChild(square);
+    };
+}
 
 const game = () => {
-    // gridSizeBtns.forEach(btn => btn.addEventListener("click", createGrid));
-    createGrid();
+    gridSizeBtns.forEach(button => {
+        button.addEventListener("click", resizeGrid);
+    });
 };
 
 game();
